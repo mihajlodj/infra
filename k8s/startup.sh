9169b37c67a4b9594d802be4e9812821365370b8
Mihajlo Đorđević <mihajlodjordjevic931@gmail.com>
2024-06-22 02:23:32 +0200

    feat: setup frontend nginx in kubernetes

--- k8s/startup.sh
+++ k8s/startup.sh
@@ -25,8 +25,21 @@ sync
 echo "********************* Initializing notification serivce *********************"
 cd ../uService-notification
 kubectl -n backend apply -f postgres-configmap.yml && kubectl -n backend apply -f postgres-secret.yml && kubectl -n backend apply -f postgres.yml && kubectl -n backend apply -f notification-configmap.yml && kubectl -n backend apply -f notification-service.yml
-sync
 echo "********************* Notification service started *********************"
+sync
+
+echo "********************* Initializing rating serivce *********************"
+cd ../uService-rating
+kubectl apply -f mongo-pvc.yml && kubectl apply -f mongo-deployment.yml && kubectl apply -f rating-service-configmap.yml && kubectl apply -f rating-service-deployment.yml
+sync
+
+echo "********************* Frontend service started *********************"
+cd ../frontend
+kubectl -n backend apply -f fe-configmap.yml && kubectl -n backend apply -f fe-deployment.yml && kubectl -n backend apply -f fe-service.yml && kubectl -n backend apply -f fe-load-balancer-service.yml 
+sync
+echo "********************* Frontend service started *********************" 
+
+echo "********************* Rating service started *********************"
 cd ../
 kubectl -n backend apply -f ingress.yml
 sync
